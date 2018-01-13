ca4e8a15e9114b18dde231189fa664e97c6a7035
Amedeo Salvati <amedeo@linux.com>
2018-01-13 18:30:03 +0100

    change default resolution to 800x600

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ class Settings():
     def __init__(self):
         """Initialize the game's settings"""
         # screen
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230,  230,  230)
         self.caption = "Amedeo version of Space Invaders"
         #kept from https://pixabay.com/it/razzo-vettoriale-spazio-lancio-2442125/
